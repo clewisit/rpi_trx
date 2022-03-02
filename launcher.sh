4512deb1c00b8c93b3ffca4533380d33bec9f0c0
Chris Lewis <clewis.it@gmail.com>
2022-03-02 12:07:08 -0800

    don't sudo

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,4 @@
 # launcher.sh
 # navigate to  home directory, then to this directory, then execute python sript, then back home
 
-cd /
-cd home/pi/FreqShow_Large
-sudo python freqshow.py
-cd /
+python3 ./freqshow.py
